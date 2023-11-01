b090d72e32faedbe577c1836b3645fe3a0b1d677
Mustafa Emre Telli <emretelli00@hotmail.com>
2023-11-01 14:52:18 +0300

    Initial Commit

---
+++ QRScanner/QRScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRScannerApp.swift
+//  QRScanner
+//
+//  Created by Emre Telli on 1.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct QRScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
